4656c761dab104eca0fc90dd49bd5a97023ab3f2
Alexey Vymyatnin <avymiatnin@aspirity.com>
2024-10-04 11:56:17 +0700

    Initial NextJs with shadcn-ui

---
+++ README.md
@@ -0,0 +1,7 @@
+// https://www.youtube.com/watch?v=GUwizGbY4cc&t=7800s&ab_channel=ArchakovBlog
+
+
+Figma design:
+// https://www.figma.com/design/cYz4fOSK74EJoqHxoNr1hT/Next-Pizza
+
+// https://github.com/Archakov06/next-pizza 
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex gap-4">
+      Home
+      <Button variant="outline">Корзина</Button>
+    </div>
+  );
+}
